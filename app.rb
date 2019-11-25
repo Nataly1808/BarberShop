41bb41834b4472f1570c6a5335131674deea5733
Nataly <nata.sokol.84@list.ru>
2019-11-25 15:49:32 +0300

    Add datetimepicker plugin

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
+	 "Users" 
+	 (
+	 "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	 "Username" TEXT,
+	 "Phone" TEXT,
+	 "datestamp" TEXT,
+	 "barber" TEXT,
+	 "color" TEXT 
+	  )'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -29,8 +44,14 @@ hh = {:username => 'Введите имя', :phone => 'Введите телеф
 if @error != ''
 	return erb :visit
 end
+db = get_db
+get_db.execute 'insert into Users (username, phone, datestamp, barber, color) values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+
 
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
+end
+def get_db
+	return SQlite3::Database.new 'barbershop.db'
 end
 
