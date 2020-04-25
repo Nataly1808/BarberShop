efc0b0b57e144e4c4238f31826ee1b268f81e858
Nataly <nata.sokol.84@list.ru>
2020-04-25 16:18:47 +0300

    Create database on app init

--- app.rb
+++ app.rb
@@ -4,17 +4,17 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 configure do
-	db = get_db
-	db.execute 'CREATE TABLE IF NOT EXISTS
-	 "Users" 
-	 (
-	 "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
-	 "Username" TEXT,
-	 "Phone" TEXT,
-	 "datestamp" TEXT,
-	 "barber" TEXT,
-	 "color" TEXT 
-	  )'
+	@db = SQlite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+		 "Users" 
+	    (
+	    "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	    "Username" TEXT,
+	    "Phone" TEXT,
+	    "datestamp" TEXT,
+	    "barber" TEXT,
+	    "color" TEXT 
+	    )'
 end
 
 get '/' do
@@ -44,14 +44,14 @@ hh = {:username => 'Введите имя', :phone => 'Введите телеф
 if @error != ''
 	return erb :visit
 end
-db = get_db
-get_db.execute 'insert into Users (username, phone, datestamp, barber, color) values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+#db = get_db
+#get_db.execute 'insert into Users (username, phone, datestamp, barber, color) values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
 
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
 end
-def get_db
-	return SQlite3::Database.new 'barbershop.db'
-end
+#def get_db
+	#return SQlite3::Database.new 'barbershop.db'
+#end
 
